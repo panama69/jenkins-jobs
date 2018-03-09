427e542e38638f9de93d576dd8b595c1b1e106e4
Dave Flynn <david.flynn@microfocus.com>
2018-03-08 18:03:45 -0600

    Update get_jobs.sh

--- util/get_jobs.sh
+++ util/get_jobs.sh
@@ -7,6 +7,12 @@
 #         stedolan.github.io/jq/download
 #
 #    inspiration for using this came from http://stackoverflow.com/questions/14843874/from-jenkins-how-do-i-get-a-list-of-the-currently-running-jobs-in-json
+#    https://stedolan.github.io/jq/
+#    jq can be installed in centos/redhat using (https://stackoverflow.com/questions/44044449/facing-issue-while-installing-jq-in-centos)
+#    yum install epel-release -y
+#    yum install jq -y
+#    jq --version
+
 # #####################
 # Resource references
 # #####################
@@ -22,6 +28,9 @@ then
    echo "The program 'jq' is currently not installed. "
    echo "You can install it by typing:"
    echo "   sudo apt install jq"
+   echo "or on centos"
+   echo "   yum install epel-release -y"
+   echo "   yum install jq -y"
    exit -1
 else
    echo "Using jq version: $res"
